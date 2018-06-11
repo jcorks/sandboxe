aff3efa32a99112bd967da726874bf762dc74693
jcorkery <jcorkery@umich.edu>
2018-06-11 11:00:14 -0400

    dev: test commit from android

---
+++ doc/entity.js
@@ -0,0 +1,14 @@
+/**
+ * @class entity
+ * @description
+ * extensible behavior object updated by the engine. Created with 
+ * {@link sandboxe.entity.create}</br></br>
+ *
+ * Entities are the base object that provides means to easily 
+ * include custom update behavior. Along with calling custom step and draw 
+ * functions over time, with {@link component}s you can provide
+ * a more manageable software platform fior youtr project. </br></br>
+ *
+ * Entities are arranged in a hierarchical fashion, where the root
+ * is assigned using {@link sandboxe.engine.setRoot} 
+ */
